373d1cc09de5bfe4e0578fbc4aae187f39587b10
zturtledog <71353802+zturtledog@users.noreply.github.com>
2022-05-15 10:22:37 -0700

    initial

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//# purpose : to contain the main renderer and host the entry
+//# contributor : ConfusedParrotfish
+
+using System.Collections;
+using System;
+using SFML.Graphics;
+using SFML.Window;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace absolutelynotaterrariaclone {
+    class Program {
+        static void Main(string[] args) {
+            window win = new window();
+            win.start(800, 600, "yay a window");
+            // win.loadsprite("D:/Imggggggss/fixedlgo.jpg");
+        }
+    }
+
+    class window {
+        RenderWindow _window;
+
+        ArrayList rndrordr;
+
+        public void start(int width, int height, string title) {
+            _window = new RenderWindow(new VideoMode((uint)width, (uint)height), "SFML window");
+            _window.SetVisible(true);
+
+            //. add event handlers
+                //# add event of which activates when closed
+            _window.Closed += new EventHandler(OnClosed); // this is very much cursed
+
+            init();
+
+            //.main update
+            while (_window.IsOpen) {
+                _window.DispatchEvents();
+                update();
+                _window.Display();
+            }
+        }
+        void OnClosed(object sender, EventArgs e) {
+            _window.Close();
+        }
+
+        public virtual void init() {
+            //
+        }
+
+        public virtual void update() {
+            //
+        }
+
+    }
+}
---
+++ formatting.md
@@ -0,0 +1,26 @@
+<!-- all bellow are optional and subject to change, these are mere sugestions that atempt to create unity across the workspace -->
+
+## comments
+
+the "//# puropse : " goes on top of the file and dictates the files main purpose
+
+the "//." clarifies the main idea of what below code does
+
+the "//#" elaborates on code puropse always found under "//."
+
+the "//:" is code that has be commented out for reason stated above in a "//#" or "//." comment
+
+the "//TODO: " indicates that something is needed to be done
+
+sign files you contributed to with "//# contributor : "
+
+## varibles
+
+varibles are all lowercase
+
+## examples
+
+varibles:
+```c#
+public house greenhouse = ((new lovelybox()).livable(verybrightgreen)).tohouse(); 
+```
